539febfe0d64283c89fa7b99110f1af5452cf006
Shrimpey304 <124566452+Shrimpey304@users.noreply.github.com>
2025-02-19 23:34:53 +0100

    feat: added header component, changed some gitignore things

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,11 @@
+import './header.css';
+
+export default function Header(){
+    return(
+        <div className='header'>
+            <div className='ndwlogo'>
+                <img src="/ndw.svg" alt="ndwlogo" />
+            </div>
+        </div>
+    );
+}
