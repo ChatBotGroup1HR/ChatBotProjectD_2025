0816e3b6aa52e488938b77a487e5a8d0a2c99fa3
Shrimpey <kimo.edelbroek@gmail.com>
2025-02-20 14:35:48 +0100

    feat: header now refers to index page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,7 +4,9 @@ export default function Header(){
     return(
         <div className='header'>
             <div className='ndwlogo'>
+                <a href='/'>
                 <img src="/ndw.svg" alt="ndwlogo" />
+                </a>
             </div>
         </div>
     );
