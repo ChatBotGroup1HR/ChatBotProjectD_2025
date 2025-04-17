692408b904c2f5f7c517c4ff966aab62d2229716
StevanIv <1070438@hr.nl>
2025-04-17 09:06:55 +0200

    chatbox area

---
+++ src/components/ui/chatbox.tsx
@@ -0,0 +1,10 @@
+import './chatbox.css';
+
+export default function Chatbox() {
+    return (
+        <div className="chatbox-container">
+            <div className="chatbox-area">
+            </div>
+        </div>
+    );
+}
