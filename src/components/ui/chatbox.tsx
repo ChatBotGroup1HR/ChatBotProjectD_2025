62f3f7e34704a4719613fab925601372c9c4d780
younesika <150244549+younesika@users.noreply.github.com>
2025-04-17 16:24:13 +0200

    chatbox
    chatbox compleet

--- src/components/ui/chatbox.tsx
+++ src/components/ui/chatbox.tsx
@@ -4,6 +4,16 @@ export default function Chatbox() {
     return (
         <div className="chatbox-container">
             <div className="chatbox-area">
+                <div className="chatbox-messages">
+                </div>
+                <div className="chatbox-input-area">
+                    <input
+                        type="text"
+                        className="chatbox-input"
+                        placeholder="Stel een vraag"
+                    />
+                    <button className="chatbox-button">➤</button>
+                </div>
             </div>
         </div>
     );
