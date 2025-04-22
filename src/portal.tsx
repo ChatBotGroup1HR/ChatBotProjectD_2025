2ab91554d3ee886b79d315a9c590ff190a2b570a
Shrimpey304 <124566452+Shrimpey304@users.noreply.github.com>
2025-04-22 14:55:42 +0200

    feat: admin portal sidebar

--- src/portal.tsx
+++ src/portal.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import './portal.css';
 import Header from './components/ui/header';
 import Footer from './components/ui/footer';
+import Sidebar from './components/ui/sidebar';
 import reportWebVitals from './reportWebVitals';
 
 const PortalBody = () => {
   return (
     <div className='portalBody'>
-      <p>Portal items</p>
+      <div className="admin-layout">
+        <Sidebar />
+        <div className="admin-content">
+          <h1>Admin Portal</h1>
+          <p>Selecteer een optie in de sidebar om de pagina te bekijken.</p>
+        </div>
+      </div>
     </div>
   );
 }
